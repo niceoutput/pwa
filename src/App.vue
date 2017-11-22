c2482b852654e736dd3b268715fecb4f8f08fd41
Mário Pereira <mario.pedro@niceoutput.com>
2017-11-22 00:36:00 +0000

    add teaser component and styles associated

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,30 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <hello></hello>
-    </main>
+    <Teaser />
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Teaser from './components/Teaser'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Teaser
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
 body {
-  margin: 0;
+  background: rgba(25, 10, 15, 1);
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
 }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
